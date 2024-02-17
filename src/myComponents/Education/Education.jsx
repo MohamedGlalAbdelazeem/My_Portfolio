9d26b07ba516a8d7552d7be524d6901e6352181c
Mohamed_glal <glal18244@gmail.com>
2024-02-17 16:25:49 +0200

    first commit

---
+++ src/myComponents/Education/Education.jsx
@@ -0,0 +1,15 @@
+import './edu.css'
+
+ 
+ export default function Education() {
+    return(
+        <section 
+        className='edu' id='education'>
+        <h1 className='titl'>Education</h1>
+        <h2><a href="https://www.eelu.edu.eg/" target='_blank' rel="noreferrer">The Egyptian E-Learning University – EELU</a></h2>
+        <h3>- Bachelor`s degree in Information technology.</h3>
+        <h3>- graduation from faculty of computer and information technology.</h3>
+        <h4>- GPA: Very Good.</h4>
+        </section>
+    );
+}
